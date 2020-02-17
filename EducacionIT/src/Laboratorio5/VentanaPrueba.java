e47b7ed601bcf66fc1290d28fba5d87927b76c53
FAMILIA <FAMILIA@10.15.87.64>
2020-02-17 16:04:24 -0300

    Creacion de una clase empleado, y prueba de una interfaz grafica en java

---
+++ EducacionIT/src/Laboratorio5/VentanaPrueba.java
@@ -0,0 +1,14 @@
+package Laboratorio5;
+
+import javax.swing.JPanel;
+
+public class VentanaPrueba extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public VentanaPrueba() {
+
+	}
+
+}
---
+++ EducacionIT/src/Laboratorio7/Empleado.java
@@ -0,0 +1,5 @@
+package Laboratorio7;
+
+public class Empleado {
+
+}
